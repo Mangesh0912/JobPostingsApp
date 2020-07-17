3814c500a9f2545cd62b49e78b53eb7bd6472d56
Mangesh <mangesh091287@gmail.com>
2020-07-17 14:47:27 +0530

    ReadMe.md updated file and build folder

--- README.md
+++ README.md
@@ -2,26 +2,27 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
-In the project directory, you can run:
+In the project directory, you can run( for dev build):
 
-### install the packages using npm install
-
+### install the packages from package.json using npm install
 ### `npm start`
 
 Runs the app in the development mode.<br />
 Open [http://localhost:8000](http://localhost:8000) to view it in the browser.
 
 The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
 
 
-### `npm run build`
+Following steps for production build:
+### 'Build the app for production'
+### `npm run build` -- creates a build folder -- already ran and checked in build folder
+### 'npm install -g serve'
+## 'serve -s build"#
+
 
 Builds the app for production to the `build` folder.<br />
 It correctly bundles React in production mode and optimizes the build for the best performance.
 
 The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
---
+++ build/precache-manifest.4dd18bd60542fa56b70191d38615fd89.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46337bc87ab69dbaf9da29bb6243969f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b85bb7abd85cabe4de47",
+    "url": "/static/css/2.ba717288.chunk.css"
+  },
+  {
+    "revision": "3f860c8cbcf1096d2fd3",
+    "url": "/static/css/main.0b9ad2a3.chunk.css"
+  },
+  {
+    "revision": "b85bb7abd85cabe4de47",
+    "url": "/static/js/2.17532d1b.chunk.js"
+  },
+  {
+    "revision": "a465e1ff3dcaabf481a774f5052b4189",
+    "url": "/static/js/2.17532d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f860c8cbcf1096d2fd3",
+    "url": "/static/js/main.73d0b389.chunk.js"
+  },
+  {
+    "revision": "5868243ba00e210be310",
+    "url": "/static/js/runtime-main.5fa19811.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
